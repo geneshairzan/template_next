8ba596825e83f48a3f308f32a281336624f12b82
genesha <genesha.irzan@gmail.com>
2023-12-28 04:54:52 +0700

    gip rebuild

--- prisma/migrations/20231227114647_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `serial_number` on the `project` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `project` DROP COLUMN `serial_number`;
