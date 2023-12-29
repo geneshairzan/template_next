8620dd71c23e9ee3dc3b56297dee8b03a1130d2d
genesha <genesha.irzan@gmail.com>
2023-12-29 15:11:34 +0700

    gip rebuild

--- src/model/projecttask.js
+++ src/model/projecttask.js
@@ -1,5 +1,17 @@
 export const projecttask = {
   label: "project task",
+  includes: { status: true },
+
+  col: [
+    { name: "title", label: "Title", w: 220 },
+    { name: "status", label: "Status", w: "auto" },
+  ],
+  datamap: (d) => {
+    return {
+      ...d,
+      status: d?.status?.name,
+    };
+  },
   form: {
     pic_id: {
       m_label: "PIC",
