47cc9bf991fa716d065abcdc7d383b1e3ff7a860
genesha <genesha.irzan@gmail.com>
2024-01-19 08:20:52 +0700

    gip rebuild

--- src/pages/api/dev/hello.js
+++
@@ -1,8 +0,0 @@
-import { pwd } from "@gh/helper/encryption";
-
-export default async function handler(req, res) {
-  res.status(200).json({
-    message: await pwd.hashing("password"),
-    isMatched: await pwd.check("password", "$2b$05$m.v1jkYLxnugEZFblFMGUOKZut/vGFx6jTIi9VxtGVDduLbfKY7D2"),
-  });
-}
