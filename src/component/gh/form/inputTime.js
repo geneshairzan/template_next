027cdfa10068f98f611471b223a74d973b6a22fb
genesha <genesha.irzan@gmail.com>
2025-08-19 06:46:16 +0700

    trim pakage

--- src/component/gh/form/inputTime.js
+++ src/component/gh/form/inputTime.js
@@ -1,15 +1,12 @@
 import * as React from "react";
 // import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 
 import Label from "./label";
 import { Stack } from "@mui/material";
 import h from "@gh/helper";
-
-import dayjs from "dayjs";
 
 export default function BasicTimePicker({
   disablePast = false,
@@ -23,13 +20,8 @@ export default function BasicTimePicker({
   return (
     <Stack spacing={0.5} width="100%">
       {!noLabel && <Label label={props.label} tip={props.tip} />}
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <TimePicker
-          fullWidth
-          renderInput={(params) => <TextField {...params} fullWidth />}
-          onChange={onChange}
-          value={props?.value || null}
-        />
+      <LocalizationProvider dateAdapter={AdapterMoment}>
+        <TimePicker fullWidth renderInput={(params) => <TextField {...params} fullWidth />} onChange={onChange} value={props?.value || null} />
       </LocalizationProvider>
     </Stack>
   );
